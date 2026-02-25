8e8d879cc14a2a3daacd25bdbc3c98c7413ccaed
zipper <thakurbhanu2924@gmail.com>
2026-02-25 21:54:19 +0530

    Updated settings and cleaned requirements for Railway

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0@fa5zv+n@(a$+pp7(a)!-q=$)i1vwz4nod$airz@wogi#9@k-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
